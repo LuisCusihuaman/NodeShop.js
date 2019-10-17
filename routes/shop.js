2f4356344a629cb0466bf5a565476afb2b8cef9f
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2019-10-17 01:49:47 -0300

    Fixed product by products controller name

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,10 @@ const path = require("path");
 
 const express = require("express");
 
+const productsController = require("../controllers/products");
+
 const router = express.Router();
 
-const productController = require("../controllers/product");
-
-router.get("/", productController.getProducts);
+router.get("/", productsController.getProducts);
 
 module.exports = router;
