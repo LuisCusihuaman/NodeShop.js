02088e43467fa54539f4c6be18b65fec3b014ea4
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2019-10-20 18:52:45 -0300

    Adding a Cart Model

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,9 +1,11 @@
 const Product = require("../models/product");
+const Cart = require("../models/cart");
+
 
 exports.getProduct = (req, res, next) => {
 	const prodId = req.params.productId;
 	Product.findById(prodId, product => {
-		res.render("shop/product-detail", { 
+		res.render("shop/product-detail", {
 			product: product,
 			pageTitle: product.title,
 			path: "/products"
@@ -31,11 +33,13 @@ exports.getIndex = (req, res, next) => {
 	});
 };
 
-exports.postCart = (req,res, next) => {
+exports.postCart = (req, res, next) => {
 	const prodId = req.body.productId;
-	console.log(`El producto id vale: ${prodId}`);
+	Product.findById(prodId, product => {
+		Cart.addProduct(prodId, product.price);
+	});
 	res.redirect("/cart");
-}
+};
 
 exports.getCart = (req, res, next) => {
 	res.render("shop/cart", { path: "/cart", pageTitle: "Your cart" });
