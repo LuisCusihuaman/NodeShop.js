f9217cf7e902057270a96d92535d1a3eb28efca6
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2019-11-06 18:16:17 -0300

    'PostAProduct, get Product/s and index' behavior is modified to use Mysql

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,8 +14,12 @@ exports.postAddProduct = (req, res, next) => {
 	const price = req.body.price;
 	const description = req.body.description;
 	const product = new Product(null, title, imageUrl, description, price);
-	product.save();
-	res.redirect("/");
+	product
+		.save()
+		.then(() => {
+			res.redirect("/");
+		})
+		.catch(err => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -43,7 +47,13 @@ exports.postEditProduct = (req, res, next) => {
 	const updatedPrice = req.body.price;
 	const updatedImageUrl = req.body.imageUrl;
 	const updateDesc = req.body.description;
-	const updateProduct  = new Product(prodId,updatedTitle,updatedImageUrl,updateDesc,updatedPrice);
+	const updateProduct = new Product(
+		prodId,
+		updatedTitle,
+		updatedImageUrl,
+		updateDesc,
+		updatedPrice
+	);
 	updateProduct.save();
 	res.redirect("/admin/products");
 };
