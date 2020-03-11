4cc83689ff59a7662c51309bbe8883f41ab4d548
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-03-11 11:06:28 -0300

    Modify the Add to Cart functionality, now the cart model use Mongoose and retrive in UI

--- models/user.js
+++ models/user.js
@@ -12,12 +12,39 @@ const userSchema = new Schema({
 	cart: {
 		items: [
 			{
-				productId: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+				productId: {
+					type: Schema.Types.ObjectId,
+					ref: "Product",
+					required: true
+				},
 				quantity: { type: Number, required: true }
 			}
 		]
 	}
 });
+
+userSchema.methods.addToCart = function(product) {
+	const cartProductIndex = this.cart.items.findIndex(cp => {
+		return cp.productId.toString() === product._id.toString();
+	});
+	let newQuantity = 1;
+	const updatedCartItems = [...this.cart.items];
+
+	if (cartProductIndex >= 0) {
+		newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+		updatedCartItems[cartProductIndex].quantity = newQuantity;
+	} else {
+		updatedCartItems.push({
+			productId: product._id,
+			quantity: newQuantity
+		});
+	}
+	const updatedCart = {
+		items: updatedCartItems
+	};
+	this.cart = updatedCart;
+	return this.save();
+};
 
 module.exports = mongoose.model("User", userSchema);
 /* const mongoConnect = require("../util/database").mongoConnect;
@@ -40,32 +67,7 @@ class User {
 	}
 
 	async addToCart(product) {
-		const cartProductIndex = this.cart.items.findIndex(cp => {
-			return cp.productId.toString() === product._id.toString();
-		});
-		let newQuantity = 1;
-		const updatedCartItems = [...this.cart.items];
 
-		if (cartProductIndex >= 0) {
-			newQuantity = this.cart.items[cartProductIndex].quantity + 1;
-			updatedCartItems[cartProductIndex].quantity = newQuantity;
-		} else {
-			updatedCartItems.push({
-				productId: new ObjectId(product._id),
-				quantity: newQuantity
-			});
-		}
-		const updatedCart = {
-			items: updatedCartItems
-		};
-		const db = mongoConnect.db();
-		return db
-			.collection("users")
-			.updateOne(
-				{ _id: new ObjectId(this._id) },
-				{ $set: { cart: updatedCart } }
-			)
-			.catch(err => console.log(err));
 	}
 
 	async getCart() {
