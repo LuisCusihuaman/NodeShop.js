e27ac5f7f16e14a476974aeb83fc27904ef7c8e6
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-03-13 18:00:13 -0300

    Now search a user id by session and store a user schema in request body

--- app.js
+++ app.js
@@ -36,7 +36,10 @@ app.use(
 );
 
 app.use((req, res, next) => {
-	User.findById("5e6853445cd2ef2960bc2971")
+	if (!req.session.user) {
+		return next();
+	}
+	User.findById(req.session.user._id)
 		.then(user => {
 			req.user = user;
 			next();
