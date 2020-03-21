83776591899470dce867e67f551839326ffae22d
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-03-21 18:36:58 -0300

    The view was set to signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,14 @@ exports.getLogin = (req, res, next) => {
 	});
 };
 
+exports.getSignup = (req, res, next) => {
+	res.render("auth/signup", {
+		path: "/signup",
+		pageTitle: "Signup",
+		isAuthenticated: false
+	});
+};
+
 exports.postLogin = (req, res, next) => {
 	User.findById("5e6853445cd2ef2960bc2971")
 		.then(user => {
@@ -19,6 +27,8 @@ exports.postLogin = (req, res, next) => {
 		})
 		.catch(err => console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
 	req.session.destroy(() => {
