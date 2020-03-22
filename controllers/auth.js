883583f8c10033cd188f1a3e1536ca66a856feb7
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-03-21 22:39:10 -0300

    Using a middleware to Protect Routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,13 +18,29 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-	User.findById("5e6853445cd2ef2960bc2971")
+	const email = req.body.email;
+	const password = req.body.password;
+	User.findOne({ email: email })
 		.then(user => {
-			req.session.isLoggedIn = true;
-			req.session.user = user;
-			req.session.save(err => {
-				res.redirect("/");
-			});
+			if (!user) {
+				return res.redirect("/login");
+			}
+			bcrypt
+				.compare(password, user.password)
+				.then(doMatch => {
+					if (doMatch) {
+						req.session.isLoggedIn = true;
+						req.session.user = user;
+						return req.session.save(() => {
+							return res.redirect("/");
+						});
+					}
+					res.redirect("/login");
+				})
+				.catch(err => {
+					console.log(err);
+					res.redirect("/login");
+				});
 		})
 		.catch(err => console.log(err));
 };
