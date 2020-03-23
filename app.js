f8c780d694f6f7fd74204b1f8622fb29ccf65658
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-03-23 20:29:55 -0300

    Added Error middleware and 500 status page error

--- app.js
+++ app.js
@@ -44,10 +44,15 @@ app.use((req, res, next) => {
 	}
 	User.findById(req.session.user._id)
 		.then(user => {
+			if (!user) {
+				return next();
+			}
 			req.user = user;
 			next();
 		})
-		.catch(err => console.log(err));
+		.catch(err => {
+			throw new Error(err);
+		});
 });
 
 app.use((req, res, next) => {
@@ -59,7 +64,13 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.use("/500", errorController.get500);
 app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+	// res.status(error.httpStatusCode).render(...);
+	res.redirect("/500");
+});
 
 mongoose
 	.connect(MONGODB_URI, {
