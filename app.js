675134ca61bad6f4bb86c2dd8d1ad3b1f179e9bf
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-03-24 12:03:27 -0300

    The images folder is allowed to be able to servers statically

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
-const csfr = require("csurf");
+const csrf = require("csurf");
 const flash = require("connect-flash");
 const multer = require("multer");
 
@@ -20,7 +20,8 @@ const store = new MongoDBStore({
 	uri: MONGODB_URI,
 	collection: "sessions"
 });
-const csrfProtection = csfr();
+const csrfProtection = csrf();
+
 const fileStorage = multer.diskStorage({
 	destination: (req, file, cb) => {
 		cb(null, "images");
@@ -49,8 +50,11 @@ const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single("image"));
+app.use(
+	multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(
 	session({
 		secret: "my secret",
@@ -89,13 +93,14 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-app.use("/500", errorController.get500);
+app.get("/500", errorController.get500);
+
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
 	// res.status(error.httpStatusCode).render(...);
 	res.status(500).render("500", {
-		pageTitle: "Error",
+		pageTitle: "Error!",
 		path: "/500",
 		isAuthenticated: req.session.isLoggedIn
 	});
