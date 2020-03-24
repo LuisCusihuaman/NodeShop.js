bd34e42fb6617ea09ac18a244cf37b4a5ce246d0
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-03-24 14:03:12 -0300

    Generated .pdf files with order data

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const path = require("path");
+
+const PDFDocument = require("pdfkit");
+
 const Product = require("../models/product");
 const Order = require("../models/order");
 
@@ -159,32 +162,57 @@ exports.getInvoice = (req, res, next) => {
 			if (!order) {
 				return next(new Error("No order found."));
 			}
-			if (order.user.userId.toString() === req.user._id.toString()) {
+			if (order.user.userId.toString() !== req.user._id.toString()) {
 				return next(new Error("Unauthorized"));
 			}
 			const invoiceName = "invoice-" + orderId + ".pdf";
 			const invoicePath = path.join("data", "invoices", invoiceName);
-			/* 
-			fs.readFile(invoicePath, (err, data) => {
-				if (err) {
-					return next(err);
-				}
-				res.setHeader("Content-Type", "application/pdf");
-				res.setHeader(
-					"Content-Disposition",
-					'inline; filename="' + invoiceName + '"'
-				);
-				res.send(data);
-			}); */
 
-			const file = fs.createReadStream(invoicePath);
+			const pdfDoc = new PDFDocument();
 			res.setHeader("Content-Type", "application/pdf");
 			res.setHeader(
 				"Content-Disposition",
 				'inline; filename="' + invoiceName + '"'
 			);
-			file.pipe(res); //forward the data that read in that stream to response,
-			// response object is writable stream , pipe (read stream ) -> writebalbe stream
+			pdfDoc.pipe(fs.createWriteStream(invoicePath));
+			pdfDoc.pipe(res);
+
+			pdfDoc.fontSize(26).text("Invoice", {
+				underline: true
+			});
+			pdfDoc.text("-----------------------");
+			let totalPrice = 0;
+			order.products.forEach(prod => {
+				totalPrice += prod.quantity * prod.product.price;
+				pdfDoc
+					.fontSize(14)
+					.text(
+						prod.product.title +
+							" - " +
+							prod.quantity +
+							" x " +
+							"$" +
+							prod.product.price
+					);
+			});
+			pdfDoc.text("---");
+			pdfDoc.fontSize(20).text("Total Price: $" + totalPrice);
+
+			pdfDoc.end();
+			// fs.readFile(invoicePath, (err, data) => {
+			//   if (err) {
+			//     return next(err);
+			//   }
+			//   res.setHeader('Content-Type', 'application/pdf');
+			//   res.setHeader(
+			//     'Content-Disposition',
+			//     'inline; filename="' + invoiceName + '"'
+			//   );
+			//   res.send(data);
+			// });
+			// const file = fs.createReadStream(invoicePath);
+
+			// file.pipe(res);
 		})
 		.catch(err => next(err));
 };
