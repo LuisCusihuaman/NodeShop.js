f860e6b4efd27ff8204b494db87dc3641fb17d7e
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-03-24 12:01:57 -0300

    Deleted validation of image and change url for path

--- routes/admin.js
+++ routes/admin.js
@@ -10,6 +10,9 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get("/add-product", isAuth, adminController.getAddProduct);
 
+// /admin/products => GET
+router.get("/products", isAuth, adminController.getProducts);
+
 // /admin/add-product => POST
 router.post(
 	"/add-product",
@@ -19,14 +22,14 @@ router.post(
 			.isLength({ min: 3 })
 			.trim(),
 		body("price").isFloat(),
-		body("description").isLength({ min: 5, max: 400 })
+		body("description")
+			.isLength({ min: 5, max: 400 })
+			.trim()
 	],
 	isAuth,
 	adminController.postAddProduct
 );
 
-// /admin/products => GET
-router.get("/products", isAuth, adminController.getProducts);
 router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
 
 router.post(
@@ -36,9 +39,10 @@ router.post(
 			.isString()
 			.isLength({ min: 3 })
 			.trim(),
-		body("imageUrl").isURL(),
 		body("price").isFloat(),
-		body("description").isLength({ min: 5, max: 400 })
+		body("description")
+			.isLength({ min: 5, max: 400 })
+			.trim()
 	],
 	isAuth,
 	adminController.postEditProduct
